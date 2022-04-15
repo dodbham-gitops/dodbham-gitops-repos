23f779d8275f870da7639a0a7922357d6c25a09b
Bryan Oliver <boliver@linux.com>
2022-04-15 16:30:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-data "vault_generic_secret" "prow" {
-  path = "secret/prow"
-}
+# data "vault_generic_secret" "prow" {
+#   path = "secret/prow"
+# }
 
 module "terraform-github-org" {
-  source = "../"
+  source = "git::https://github.com/olivercodes/tf-github-org?ref=v0.0.1"
   repositories   = var.repositories
   org_config     = var.org_config
   owners_aliases = var.owners_aliases
