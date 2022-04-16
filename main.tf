9d99d312e8be8230f9d880d0eb83e2c9516d9f68
Bryan Oliver <boliver@linux.com>
2022-04-16 13:38:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "terraform-github-org" {
-  source = "git::https://github.com/olivercodes/tf-github-org?ref=v0.0.5"
+  source = "git::https://github.com/olivercodes/tf-github-org?ref=v0.0.6"
   repositories   = var.repositories
   org_config     = var.org_config
   owners_aliases = var.owners_aliases
